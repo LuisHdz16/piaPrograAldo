eb2e1932c1f1392e992f939a0ef627a6a6f60578
LuisHdz16 <luishernandez16141@gmail.com>
2025-04-03 19:16:01 -0600

    first commit

---
+++ pia/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace pia
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ pia/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace pia
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
